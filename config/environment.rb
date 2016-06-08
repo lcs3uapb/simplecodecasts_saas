589207cd03b6e61eb538b65176efacbef0f40394
Leon Smith <lcs3.uapb@gmail.com>
2016-06-08 01:54:54 +0000

    Comment out last changes

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
+#config.action_mailer.raise_delivery_errors = true
+#config.action_mailer.delivery_method = :smtp
 ActionMailer:Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
@@ -16,4 +16,4 @@ ActionMailer:Base.smtp_settings = {
     :enable_startstls_auto => true
 }
 
-config.action_mailer.default_url_options = { :host => 'calm-bayou-41264.herokuapp.com' }
+#config.action_mailer.default_url_options = { :host => 'calm-bayou-41264.herokuapp.com' }
